1f61682053ec8d84af0a43eb6a6c6f39902f88af
13tonydinero <13tonydinero@gmail.com>
2021-10-05 12:14:21 +0200

    style faqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {About} from '../styles';
+import styled from 'styled-components';
+
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -11,6 +14,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime assumenda, quos dolore amet, exercitationem aperiam voluptate, architecto pariatur saepe laudantium in officia! Similique temporibus magnam soluta maxime quidem omnis.</p>
                 </div>
             </div>
+            <div className="faq-line"></div>
             <div className="question">
                 <h4>What Products do you offer?</h4>
                 <div className="answer">
@@ -18,6 +22,8 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime assumenda, quos dolore amet, exercitationem aperiam voluptate, architecto pariatur saepe laudantium in officia! Similique temporibus magnam soluta maxime quidem omnis.</p>
                 </div>
             </div>
+            <div className="faq-line"></div>
+
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
@@ -25,6 +31,8 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime assumenda, quos dolore amet, exercitationem aperiam voluptate, architecto pariatur saepe laudantium in officia! Similique temporibus magnam soluta maxime quidem omnis.</p>
                 </div>
             </div>
+            <div className="faq-line"></div>
+
             <div className="question">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
@@ -32,8 +40,38 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime assumenda, quos dolore amet, exercitationem aperiam voluptate, architecto pariatur saepe laudantium in officia! Similique temporibus magnam soluta maxime quidem omnis.</p>
                 </div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #ccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 3rem 0rem;
+        font-weight: 100;
+
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
+
 
 export default FaqSection;
